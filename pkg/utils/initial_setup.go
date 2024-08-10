86b0239f79941a5eb567d15917c09eaed05bab04
nivicius <niviciusdev@gmail.com>
2024-08-09 22:20:04 -0300

    updated print function output

--- pkg/utils/initial_setup.go
+++ pkg/utils/initial_setup.go
@@ -9,6 +9,8 @@ import(
 const (
     Red   = "\033[31m"
     Black = "\033[1;30m"
+    Green = "\033[1;32m"
+    Magenta = "\033[1;35m"
     Reset = "\033[0m"
 )
 
@@ -18,11 +20,11 @@ func Colorize(color string, text string) string {
 }
 
 func GetToken() string {
-  personalGithubToken := os.Getenv("personal_github_token")
+  personalGithubToken := os.Getenv("personal_github_toke")
   if personalGithubToken == "" {
-    fmt.Println(Colorize(Red, `Error: 'personal_github_token' environment variable not set.
-
-      To resolve this:
+    fmt.Println(Colorize(Red, "Error: 'personal_github_token' environment variable not set."))
+    fmt.Println(Colorize(Magenta, "To resolve this: "))
+    fmt.Println(Colorize(Green, `
       1. Generate a GitHub personal access token with the 'user:follow' and 'read:user' scopes at https://github.com/settings/tokens.
       2. Set the token in your environment with:
       export personal_github_token="your_token_here"
